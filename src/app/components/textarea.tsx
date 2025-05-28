aaa4ca9a17d829e397395318cf2b3571af7ade7a
ehrro <dejan.eric@outlook.com>
2025-05-28 10:51:58 +0200

    Added error handling for textarea

--- src/app/components/textarea.tsx
+++ src/app/components/textarea.tsx
@@ -1,5 +1,6 @@
 import { countWords } from "@/helpers/form-validation";
 import { Label } from "./input";
+import { useEffect, useState } from "react";
 
 export function Textarea({
   label,
@@ -24,23 +25,39 @@ export function Textarea({
 }) {
   const id = `textarea-${name.replace(/\s+/g, "-").toLowerCase()}`;
   //calc rows based on min and max
-  const calculatedRows = Math.ceil((max - min) / 30); // Assuming 10 characters per row
+  const calculatedRows = Math.ceil((max - min) / 20); // Assuming 10 characters per row
   const adjustedRows = Math.max(rows, calculatedRows); // Ensure at least the default rows
+
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  useEffect(() => {
+    const wordCount = countWords(value);
+    setIsInvalid(wordCount < min || wordCount > max);
+  }, [value, min, max]);
 
   return (
     <div className="flex flex-col gap-2 w-full items-start">
       <Label label={label} required={required} />
-      <div className="flex flex-col gap-2 w-full items-start">
-        <textarea
-          className="border border-transparent bg-lightgray focus:bg-white focus:border-gray-300 py-2 px-3 w-full resize-none"
-          value={value}
-          id={id}
-          name={name}
-          onChange={(e) => onChange(e.target.value)}
-          placeholder={placeholder}
-          rows={adjustedRows}
-          maxLength={countWords(value) >= max ? max : undefined}
-        />
+      <div className="flex flex-col gap-2 w-full items-start ">
+        <div className="relative w-full">
+          <textarea
+            className={`border bg-lightgray focus:bg-white py-2 px-3 w-full resize-none ${
+              isInvalid
+                ? "border-red-500 focus:border-red-500"
+                : "focus:border-gray-300 border-transparent "
+            }`}
+            value={value}
+            id={id}
+            name={name}
+            onChange={(e) => onChange(e.target.value)}
+            placeholder={placeholder}
+            rows={adjustedRows}
+            maxLength={countWords(value) >= max ? max : undefined}
+          />
+          <span className="text-sm text-gray-700 absolute right-6 bottom-3 bg-white px-1.5 rounded">
+            {countWords(value)} / {max} words
+          </span>
+        </div>
         <p className="text-sm text-gray-500">
           This question requires {min} words minimum to {max} words maximum.
         </p>
